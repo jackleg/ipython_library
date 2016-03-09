121b76068d6ece4c177a8bbf0d1e1eddd5d909c7
한승균 <jay.han@daumkakao.com>
2016-03-09 17:15:38 +0900

    HTML 함수와 util 모듈을 기본 import.

--- 00-common-start.py
+++ 00-common-start.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython.core.display import HTML
 
 # packages by jay.han
 import strutil
 import statsutil
 import dfutil
+import util
 
 plt.style.use('ggplot')
