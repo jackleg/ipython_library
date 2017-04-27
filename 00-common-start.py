6331593f8a0bd6909cf75501dd7fbe41b78bfd92
1003946 <1003946@SKP1003946MN001.local>
2017-04-27 11:44:09 +0900

    ipython memory usage watching 시작.

--- 00-common-start.py
+++ 00-common-start.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from IPython.core.display import HTML
 
+import ipython_memory_usage.ipython_memory_usage as imu
+
 # packages by jay.han
 from putils import *
 
 plt.style.use('ggplot')
+imu.start_watching_memory()
